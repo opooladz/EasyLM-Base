c05d78825a7822e2f5e01f9eacf519cd8ac8638d
jaisong123 <40257549+jaisong123@users.noreply.github.com>
2024-09-25 12:56:13 -0400

    Update pretrain_llama_7b.sh
    changed to jsg bucket

--- examples/pretrain_llama_7b.sh
+++ examples/pretrain_llama_7b.sh
@@ -63,6 +63,6 @@ python -m EasyLM.models.llama.llama_train \
     --logger.online=True \
     --logger.prefix='EasyLM' \
     --logger.project="llama2_3b" \
-    --logger.output_dir="gs://jg-bucket/LLM/easyLM/llama2_3b/experiment_output/llama3-log" \
+    --logger.output_dir="gs://jsg-bucket/LLM/easyLM/llama2_3b/experiment_output/llama3-log" \
     --logger.wandb_dir="/dev/shm/experiment_output/open_llama2_3b" \
 |& tee $HOME/output.txt
