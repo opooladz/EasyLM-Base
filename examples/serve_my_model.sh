c8d753f8531fbd91e582f734876895d106ebae77
jaisong123 <40257549+jaisong123@users.noreply.github.com>
2024-11-24 01:51:43 -0500

    adj

--- examples/serve_my_model.sh
+++ examples/serve_my_model.sh
@@ -3,15 +3,13 @@
 umask 000
 LEV_ROOT=$(dirname "$(readlink -f $0)")/..
 
-source ~/venv/bin/activate
-
 PYTHONPATH=${LEV_ROOT}:${LEV_ROOT}/src:${LEV_ROOT}/examples:$PYTHONPATH "$@"
 
 # TPU specific flags to improve throughput
 export LIBTPU_INIT_ARGS='--xla_jf_spmd_threshold_for_windowed_einsum_mib=0 --xla_tpu_spmd_threshold_for_allgather_cse=10000 --xla_enable_async_all_gather=true --xla_tpu_enable_latency_hiding_scheduler=true TPU_MEGACORE=MEGACORE_DENSE'
 
 python -m EasyLM.models.llama.llama_serve \
-    --mesh_dim='1,-1,1' \
+    --mesh_dim='-1,8,1' \
     --dtype='bfloat16' \
     --llama.base_model='llama2_3b' \
     --load_checkpoint='trainstate_params::gs://jsg-bucket/LLM/easyLM/llama2_3b/experiment_output/llama3-log/020dde1f9ea5464981aa022d89d09cff/streaming_train_state' \
