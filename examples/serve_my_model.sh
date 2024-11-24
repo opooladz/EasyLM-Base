b643f24d85eabd2ed654c54225d058bc244efaff
jaisong123 <40257549+jaisong123@users.noreply.github.com>
2024-11-24 01:48:07 -0500

    adjust serving script

--- examples/serve_my_model.sh
+++ examples/serve_my_model.sh
@@ -1,12 +1,21 @@
 #! /bin/bash
 
-# Allow external connections by binding to 0.0.0.0 instead of localhost
+umask 000
+LEV_ROOT=$(dirname "$(readlink -f $0)")/..
+
+source ~/venv/bin/activate
+
+PYTHONPATH=${LEV_ROOT}:${LEV_ROOT}/src:${LEV_ROOT}/examples:$PYTHONPATH "$@"
+
+# TPU specific flags to improve throughput
+export LIBTPU_INIT_ARGS='--xla_jf_spmd_threshold_for_windowed_einsum_mib=0 --xla_tpu_spmd_threshold_for_allgather_cse=10000 --xla_enable_async_all_gather=true --xla_tpu_enable_latency_hiding_scheduler=true TPU_MEGACORE=MEGACORE_DENSE'
+
 python -m EasyLM.models.llama.llama_serve \
-    --load_llama_config='3b' \
+    --mesh_dim='1,-1,1' \
+    --dtype='bfloat16' \
+    --llama.base_model='llama2_3b' \
     --load_checkpoint='trainstate_params::gs://jsg-bucket/LLM/easyLM/llama2_3b/experiment_output/llama3-log/020dde1f9ea5464981aa022d89d09cff/streaming_train_state' \
     --tokenizer='openlm-research/open_llama_3b_v2' \
-    --mesh_dim='1,-1,1' \
-    --dtype='bf16' \
     --input_length=1024 \
     --seq_length=2048 \
     --lm_server.batch_size=4 \
